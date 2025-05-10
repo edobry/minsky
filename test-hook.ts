5515218a82b022dacb2dfc8f89733827b7c41a43
Eugene Dobry <eugene@dobry.me>
2025-05-10 19:44:10 -0400

    Test pre-commit hook with non-fixable error

--- test-hook.ts
+++ test-hook.ts
@@ -1,3 +1,6 @@
 // This is a test file for checking pre-commit hooks.
-const testVariable = "This should cause a lint error";
+const testVariable = "This was fixed";
 console.log(testVariable);
+
+// This should cause a more severe lint error
+console.log(undeclaredVariable); // Using an undeclared variable
