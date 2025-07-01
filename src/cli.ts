b68810452e1183e5adb96c0de43c55b982576b12
Eugene Dobry <eugene@dobry.me>
2025-07-01 12:45:17 -0400

    fix: Remove debug console.log statements from CLI
    - Remove hardcoded console.log debug statements that appeared in all CLI commands
    - CLI now has clean output without unwanted debug information
    - Debug statements were appearing regardless of debug mode setting
    - Fixes noisy CLI output for normal usage

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env bun
 
-console.log("DEBUG: CLI module loading");
 import { Command } from "commander";
 import { log } from "./utils/logger.js";
 import { exit } from "./utils/process.js";
@@ -48,13 +47,8 @@ export async function createCli(): Promise<Command> {
  * This is only executed when this file is run directly
  */
 async function main(): Promise<void> {
-  console.log("DEBUG: main() called");
-  console.log("DEBUG: Bun.argv:", Bun.argv);
   await createCli();
-  console.log("DEBUG: createCli() completed");
-  console.log("DEBUG: About to call parseAsync");
   await cli.parseAsync(Bun.argv);
-  console.log("DEBUG: parseAsync completed");
 }
 
 // Run the CLI
