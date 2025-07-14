f33eed0d7eac358e257a386f392f98802b74e4a3
Eugene Dobry <eugene@dobry.me>
2025-07-14 18:29:40 -0400

    amended commit

--- src/domain/prepared-merge-commit-workflow.test.ts
+++ src/domain/prepared-merge-commit-workflow.test.ts
@@ -11,9 +11,9 @@ const TEST_ARRAY_SIZE = 3;
  * create regular PR branches instead of prepared merge commits.
  */
 import { describe, test, expect, beforeEach } from "bun:test";
-import { GitService } from "../git";
-import { sessionPrFromParams } from "../session";
-import { createMock, setupTestMocks } from "../../utils/test-utils/mocking";
+import { GitService } from "./git";
+import { sessionPrFromParams } from "./session";
+import { createMock, setupTestMocks } from "../utils/test-utils/mocking";
 import { log } from "../utils/logger";
 
 // Set up automatic mock cleanup
