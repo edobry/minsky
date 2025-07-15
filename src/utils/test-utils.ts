88b76b2f058f338e7188546539860db3d6f777a9
Eugene Dobry <eugene@dobry.me>
2025-07-15 16:09:14 -0400

    fix: use test utilities instead of creating temp dirs in project root
    - Updated rules-format.test.ts to use createCleanTempDir() utility
    - Updated quoting.test.ts to use createCleanTempDir() utility
    - Prevents test-rules-tmp and test-verification-tmp from being created in repo root
    - Uses proper test isolation with automatic cleanup registration
    - Follows best practices for test directory management

--- src/utils/test-utils.ts
+++ src/utils/test-utils.ts
@@ -58,7 +58,6 @@ export function mockDateFunctions(fixedDate = TEST_TIMESTAMPS.FIXED_DATE) {
   } as unknown as DateConstructor;
 
   // Copy all the static methods from the original Date
-<<<<<<< HEAD
   MockDate.now = () => fixedDateTime;
   MockDate.parse = originalDate.parse;
   MockDate.UTC = originalDate.UTC;
@@ -69,24 +68,6 @@ export function mockDateFunctions(fixedDate = TEST_TIMESTAMPS.FIXED_DATE) {
   // Return function to restore the original Date
   return () => {
     global.Date = originalDate;
-=======
-  if (MockDate) {
-    MockDate.now = () => fixedDateTime;
-    MockDate.parse = originalDate?.parse;
-    MockDate.UTC = originalDate?.UTC;
-  }
-
-  // Replace global Date
-  if (global) {
-    global.Date = MockDate;
-  }
-
-  // Return function to restore the original Date
-  return () => {
-    if (global) {
-      global.Date = originalDate;
-    }
->>>>>>> origin/main
   };
 }
 
