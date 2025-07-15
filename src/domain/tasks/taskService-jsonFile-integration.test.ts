15cf45497cdceb176566c3e4dd82b241111dda78
Eugene Dobry <eugene@dobry.me>
2025-07-14 20:28:29 -0400

    fix(task#280): Fix import paths in taskService test files
    - Fix taskService.test.ts imports to use same-directory paths (./taskService, ./taskBackend)
    - Fix taskService-jsonFile-integration.test.ts imports to use same-directory paths
    - These fixes resolve module resolution errors and make tests pass
    
    Progress: 1/60+ test files with import issues fixed

--- src/domain/tasks/taskService-jsonFile-integration.test.ts
+++ src/domain/tasks/taskService-jsonFile-integration.test.ts
@@ -5,8 +5,8 @@ const TEST_VALUE = 123;
  */
 import { describe, test, expect, beforeEach } from "bun:test";
 import { join } from "path";
-import { TaskService } from "../taskService";
-import { createJsonFileTaskBackend } from "../jsonFileTaskBackend";
+import { TaskService } from "./taskService";
+import { createJsonFileTaskBackend } from "./jsonFileTaskBackend";
 import { setupTestCleanup } from "../../../utils/test-utils/cleanup";
 import { setupEnhancedMocking } from "../../../utils/test-utils/enhanced-mocking";
 import { testDataFactory, DatabaseIsolation } from "../../../utils/test-utils/test-isolation";
