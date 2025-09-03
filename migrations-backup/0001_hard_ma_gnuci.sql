5cf77c277e6bcf1bd4c75df0f632a6c9c7dd3f52
Eugene Dobry <eugene@dobry.me>
2025-09-03 17:07:01 -0400

    chore: clean up root

--- migrations-backup/0001_hard_ma_gnuci.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "task_embeddings" (
-	"id" text PRIMARY KEY NOT NULL,
-	"task_id" text,
-	"dimension" integer NOT NULL,
-	"metadata" jsonb,
-	"created_at" timestamp with time zone DEFAULT now(),
-	"updated_at" timestamp with time zone DEFAULT now()
-);
--- test.test.ts
+++
@@ -1,9 +0,0 @@
-import { mock } from "bun:test";
-
-const mockLog = {
-  info: mock(() => {}),
-  debug: mock(() => {}),
-  warn: mock(() => {}),
-  error: mock(() => {}),
-  cli: mock(() => {}),
-};
